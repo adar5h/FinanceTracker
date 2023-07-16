69a9069ea06a6c318533889cfea5d26839575875
Adarsh <iadarshh@gmail.com>
2023-07-17 00:32:20 +0530

    added the option to track stock from FE

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,10 @@
 class UserStocksController < ApplicationController
+
+    def create
+        stock = Stock.check_db(params[:ticker]) # check if the stock already exists in the table
+        stock = Stock.new_lookup(params[:ticker]) if stock.nil? #make the API call if it doesn't
+        @user_stock = UserStock.create(user: current_user, stock: stock)
+        flash[:notice] = "Stock #{stock.name} has been successfully added to your portfolio"
+        redirect_to my_portfolio_path
+    end
 end
