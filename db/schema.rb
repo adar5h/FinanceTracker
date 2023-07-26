7594bdf77ada8d8fbc8c09de065b05950d49f3b8
Adarsh <iadarshh@gmail.com>
2023-07-27 00:20:52 +0530

    added user first & last name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_15_073720) do
+ActiveRecord::Schema.define(version: 2023_07_22_134832) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2023_07_15_073720) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
