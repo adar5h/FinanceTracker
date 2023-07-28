6967f6072bae9141a0c5fdacaec2e8abc4f42cb7
Adarsh <iadarshh@gmail.com>
2023-07-29 00:45:06 +0530

    added friends list view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
+  
   def my_portfolio
     @tracked_stocks = current_user.stocks
+  end
+
+  def friends
+    @friends = current_user.friends
+  end
+
+  def search
+    # @friend = User.find_by(params[:])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
-  get 'search_stocks', to: 'stocks#search' 
+  get 'search_stocks', to: 'stocks#search'
+  get 'search_friends', to: 'users#search'
+  get 'friends', to: 'users#friends' 
 end
